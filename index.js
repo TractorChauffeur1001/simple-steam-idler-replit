81e5959af5b62bd844e706c10467313fedd49ac0
TractorChauffeur1001 <158787390+TractorChauffeur1001@users.noreply.github.com>
2024-02-04 01:55:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var username = process.env.username;
 var password = process.env.password;
 var shared_secret = process.env.shared;
 
-var games = [730, 440, 570];  // Enter here AppIDs of the needed games
+var games = [730, 252490, 271590, 359550, 851850];  // Enter here AppIDs of the needed games
 var status = 1;  // 1 - online, 7 - invisible
 
 
